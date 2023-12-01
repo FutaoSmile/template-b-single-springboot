aac943d7ef78c2d44101f1414e11ef70af1e3df4
futao <futao@mysteel.com>
2023-12-01 11:03:13 +0800

    增加web模块

--- src/main/java/com/idle/fish/template/b/single/springboot/SingleSpringBootApplication.java
+++ src/main/java/com/idle/fish/template/b/single/springboot/SingleSpringBootApplication.java
@@ -2,11 +2,13 @@ package com.idle.fish.template.b.single.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * @author idle fish
  * @since 2023/11/21
  */
+@ServletComponentScan("com.idle.fish.template.b.single.springboot.filter")
 @SpringBootApplication
 public class SingleSpringBootApplication {
     public static void main(String[] args) {
--- src/main/java/com/idle/fish/template/b/single/springboot/controller/DemoController.java
+++ src/main/java/com/idle/fish/template/b/single/springboot/controller/DemoController.java
@@ -1,22 +1,27 @@
 package com.idle.fish.template.b.single.springboot.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author idle fish
  * @since 2023/11/21
  */
+@Slf4j
 @RequestMapping("/demo")
 @RestController
 public class DemoController {
 
     @GetMapping("/test")
-    public List<String> test() {
+    public List<String> test(@RequestBody Map<String,Object> map) {
+        System.out.println(map);
         List<String> ret = new ArrayList<>();
         ret.add("hello");
         ret.add("world");
